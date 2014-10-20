35013836968d0ea96d13c8cad0c139bf2e66daa5
zackattacknyu <zackattacknyu@gmail.com>
2014-10-20 00:54:46 -0700

    updated theory figure

--- matlab/permsScript.m
+++ matlab/permsScript.m
@@ -9,17 +9,18 @@ if green, divisible by 5
 %}
 
 %this is the set of data units as ARs described above
-v = [2 3 10 15 30 5 5];
+%v = [2 3 10 15 30 5 5];
+v = [10 30 6 15];
 pv = perms(v);
 
-seekTimes = zeros(5040,1);
+seekTimes = zeros(24,1);
 
-for index = 1:5040
+for index = 1:24
    seekTimes(index) = getEST(pv(index,:));
 end
 
 [M,I] = min(seekTimes);
 
-minInds = find(seekTimes==9);
+minInds = find(seekTimes==7);
 
 goodArrangements = pv(minInds,:);
